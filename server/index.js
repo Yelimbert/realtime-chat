69ce17523c83a9bc6e59ba4a9908b32d0ba47261
Yelimbert Javier <yelimbertjavier@MacBook-Pro-de-Yelimbert.local>
2022-11-06 00:47:58 -0400

    starting

---
+++ server/index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const cors = require("cors");
+const { Server } = require("socket.io");
+const port = 3001;
+
+app.use(cors());
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+    cors: {
+        origin: "http://localhost:3000",
+        methods: ["GET", "POST"],
+    },
+});
+
+io.on("connection", (socket) => {
+    console.log(`User connected: ${socket.id}`);
+
+    socket.on("disconnect", () => {
+        console.log(`User disconnected: ${socket.id}`)
+    })
+})
+
+server.listen(port, () => {
+    console.log(`Server running at port: ${port}`);
+});
